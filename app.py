0471776185ca4caa49256b01422bdfd4b7e83275
Florentin Hortet <fhortet.petwag@gmail.com>
2021-10-26 13:42:22 +0100

    [CLEAN] add breaklines

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from closest_price.loader import Loader
 from closest_price.closest_price import the_price_is_right
+
+
 def main():
     values = Loader.load_input()
 
     target = int(input("Choisissez un prix cible: "))
 
     print(the_price_is_right(values, target))
+
 
 if __name__ == "__main__":
     main()
